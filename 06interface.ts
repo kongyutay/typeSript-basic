1ace6168790640c01c0ec3544307ac65c31da920
Tay Kong Yu <kongyutay@gmail.com>
2024-07-17 23:18:41 +0800

    difference between interface and custom typename

--- 06interface.ts
+++ 06interface.ts
@@ -10,3 +10,19 @@ let person2: IPerson = {
     age: 19,
     sayHi(){}
 }
+
+//接口和类型别名之间的区别：接口只可以给对象指定类型，而类型别名可以为任意类型指定别名
+interface IPerson {
+    name: string
+    age: number
+    sayHi(): void
+}
+
+//类型别名相当于声明变量，要等号=
+type IPerson2 = {
+    name: string
+    age: number
+    sayHi(): void
+}
+
+type NumStr = number | string
